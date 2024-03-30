23d1e0008174f288d427876bfa3654a1fa492421
ppg <405954049@qq.com>
2024-03-30 12:04:10 +0800

    初始化脚手架

---
+++ src/main/java/com/example/myapp/web/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.example.myapp.web.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @Value("${spring.application.name}")
+    private String application;
+
+
+    @GetMapping(value = "/health")
+    public String health() {
+        return "{\"status\":\"UP\",\"appName\":\"" + application + "\"}";
+    }
+}
