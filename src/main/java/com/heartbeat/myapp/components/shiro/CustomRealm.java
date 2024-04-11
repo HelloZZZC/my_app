172a403d0c96cd514cc96746c9f3c5c27cbf9528
ppg <405954049@qq.com>
2024-04-11 17:54:01 +0800

    Shiro Custom Realm

--- src/main/java/com/heartbeat/myapp/components/shiro/CustomRealm.java
+++ src/main/java/com/heartbeat/myapp/components/shiro/CustomRealm.java
@@ -1,5 +1,8 @@
 package com.heartbeat.myapp.components.shiro;
 
+import com.heartbeat.myapp.biz.StaffService;
+import com.heartbeat.myapp.dp.identifier.StaffId;
+import com.heartbeat.myapp.dto.StaffDTO;
 import com.heartbeat.myapp.repository.StaffRepository;
 import com.heartbeat.myapp.util.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -11,13 +14,18 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @Slf4j
 public class CustomRealm extends AuthorizingRealm {
 
-    private StaffRepository staffRepository;
+    @Autowired
+    private StaffService staffService;
 
     /**
      * 授权
@@ -28,7 +36,19 @@ public class CustomRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         Integer staffId = (Integer) principalCollection.getPrimaryPrincipal();
+        StaffDTO staffDTO = staffService.getStaff(new StaffId(staffId));
+
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        // 设置角色
+        Set<String> roles = new HashSet<>();
+        roles.add(staffDTO.getRoleDTO().getCode());
+        info.setRoles(roles);
+        // 设置权限
+        Set<String> permissions = new HashSet<>();
+        staffDTO.getRoleDTO().getPermissions().forEach(permissionDTO -> {
+            permissions.add(permissionDTO.getCode());
+        });
+        info.setStringPermissions(permissions);
 
         return info;
     }
